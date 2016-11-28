2f65e202cab7adff10357f63bb0746c91fa02c0b
Matineh Akhlaghinia <matineh.akhlaghinia.15@ucl.ac.uk>
2016-11-28 03:26:20 +0000

    booking endpoint

--- start/gymBooking/lib/classesRepository.js
+++ start/gymBooking/lib/classesRepository.js
@@ -1,13 +1,17 @@
 'use strict';
 
 var classes = require('./classes.json');
-var jp = require('jsonpath')
+var bookedClasses = require('./bookedClasses.json');
+var jp = require('jsonpath');
 
 module.exports = {
-    get: function (id) {
-        return jp.query(classes, '$..[?(@.id=='+id+')]'); //????
-    },
-    all: function () {
-        return classes;
-    }
+  get: function (id) {
+    return jp.query(classes, '$..[?(@.id=='+id+')]'); //????
+  },
+  all: function () {
+    return classes;
+  },
+  booked: function() {
+    return bookedClasses;
+  }
 };
