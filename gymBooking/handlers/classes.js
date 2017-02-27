e0f2b885532ef33d49dd8d97208c1aceabd124e1
Matineh Akhlaghinia <matineh.akhlaghinia.15@ucl.ac.uk>
2017-02-27 00:41:01 +0000

    minor changes to the code

--- gymBooking/handlers/classes.js
+++ gymBooking/handlers/classes.js
@@ -1,10 +1,7 @@
  'use strict';
 
-var repository = require('../lib/classesRepository');
+
 var Connection = require('tedious').Connection;
-
-
-
 var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
 var config = {
